fd0c7bcaf2818f6e36426aac328a34e92b3b23c2
Sandeep Verma <sandeepverma4321@gmail.com>
2023-08-03 15:53:54 +0530

    04. Added the Route in RouteConfig for Student controller

--- MVC WebApplication/App_Start/RouteConfig.cs
+++ MVC WebApplication/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVC_WebApplication
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Student", action = "Student", id = UrlParameter.Optional }
             );
         }
     }
